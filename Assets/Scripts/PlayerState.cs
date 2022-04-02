6d4bcd9461457be295dfb617f634699340c1ec2d
Shravan Kumar <shravan.k55@protonmail.com>
2022-04-02 14:55:53 -0700

    Added code to pick up a screwdriver in the world.

---
+++ Assets/Scripts/PlayerState.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    public bool hasScrewdriver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hasScrewdriver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddScrewdriver()
+    {
+        hasScrewdriver = true;
+    }
+
+    public void RemoveScrewdriver()
+    {
+        hasScrewdriver = false;
+    }
+}
