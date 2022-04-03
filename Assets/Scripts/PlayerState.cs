4f7a05c6446ce01ead41846ad692d4959793059a
Shravan Kumar <shravan.k55@protonmail.com>
2022-04-03 13:47:07 -0700

    Added minor fix to AI.

--- Assets/Scripts/PlayerState.cs
+++ Assets/Scripts/PlayerState.cs
@@ -10,6 +10,11 @@ public class PlayerState : MonoBehaviour
     public GameObject overworldTriggerBox;
     private TriggerFlag overworldTriggerFlag;
 
+    // Doors
+    public Transform[] overworldDoorTransforms;
+    public Transform[] underworldDoorTransforms;
+    public Transform closestDoor;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +26,43 @@ public class PlayerState : MonoBehaviour
     void Update()
     {
         isInOverworld = overworldTriggerFlag.getFlag();
+
+        if (isInOverworld)
+        {
+            if (overworldDoorTransforms.Length > 0)
+            {
+                closestDoor = overworldDoorTransforms[0];
+                float closestDist = Vector3.Distance(closestDoor.position, transform.position);
+
+                for (int i = 0; i < overworldDoorTransforms.Length; i++)
+                {
+                    float dist = Vector3.Distance(overworldDoorTransforms[i].position, transform.position);
+                    if (dist < closestDist)
+                    {
+                        closestDist = dist;
+                        closestDoor = overworldDoorTransforms[i];
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (underworldDoorTransforms.Length > 0)
+            {
+                closestDoor = underworldDoorTransforms[0];
+                float closestDist = Vector3.Distance(closestDoor.position, transform.position);
+
+                for (int i = 0; i < underworldDoorTransforms.Length; i++)
+                {
+                    float dist = Vector3.Distance(underworldDoorTransforms[i].position, transform.position);
+                    if (dist < closestDist)
+                    {
+                        closestDist = dist;
+                        closestDoor = underworldDoorTransforms[i];
+                    }
+                }
+            }
+        }
     }
 
     public void AddScrewdriver()
